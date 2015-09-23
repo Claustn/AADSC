dbdff2550898ec6d26b03bd64e3e9258c6a27827
Claustn <claustn@gmail.com>
2015-09-23 09:24:44 +0200

    First Upload

---
+++ MyCompany.ps1
@@ -0,0 +1,12 @@
+﻿#requires -Version 4
+Configuration MyCompany {
+    Node 'Telnet'
+    {
+        #Use the Windows Feature resource to add Telnet-Client
+        WindowsFeature TelnetClient
+        {
+            Ensure = 'Present'
+            Name = 'Telnet-Client'
+        }
+    }
+}
