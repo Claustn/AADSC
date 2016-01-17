2ab4d30ac2ee36e09d8359d44a2ca251e83f57e7
Claustn <claustn@gmail.com>
2016-01-17 11:14:04 +0100

    Upload DSC json file

---
+++ MyOtherCompany.ps1
@@ -0,0 +1,11 @@
+﻿Configuration MyOtherCompany {
+    Node 'Telnet'
+    {
+        #Use the Windows Feature resource to add Telnet-Client
+        WindowsFeature TelnetClient
+        {
+            Ensure = 'Present'
+            Name = 'Telnet-Client'
+        }
+    }
+} 
